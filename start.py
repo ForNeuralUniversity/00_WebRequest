0813659141838e03466434c8f0495d5ce00c5c0d
Scorpunduk <scorpunduk@yandex.ru>
2022-08-18 14:19:59 +0300

    01 - Json data getter

--- start.py
+++ start.py
@@ -1,2 +1,9 @@
 import requests
-import flask
+import json
+
+url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+# почитать - https://www.cbr-xml-daily.ru/
+response = requests.get(url)
+data = json.loads(response.text)
+print(response)
+print(data)
